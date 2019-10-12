ffbfed9acd05d7a8dab7b1443939db8b0a59c32b
aaronamk <amklein@email.wm.edu>
2019-10-12 17:05:12 -0400

    Added collides method and dimension setters

--- include/actor.h
+++ include/actor.h
@@ -2,23 +2,29 @@
 #define ACTOR_H
 
 class Actor {
-	protected:
-		double x, y, x_speed, y_speed, width, height;
+    protected:
+        double x, y, x_speed, y_speed, width, height;
 
-	public:
-		Actor(double x, double y, double width, double height);
-		
-		virtual void run(void);
-		
-		virtual void reset(void) = 0;
-		
-		double get_x(void);
-		
-		double get_y(void);
-		
-		double get_x_speed(void);
-		
-		double get_y_speed(void);
+    public:
+        Actor(double x, double y, double width, double height);
+        
+        virtual void run(void);
+        
+        virtual void reset(void) = 0;
+        
+        double get_x(void);
+        
+        double get_y(void);
+        
+        double get_width(void);
+
+        double get_height(void);
+
+        double get_x_speed(void);
+        
+        double get_y_speed(void);
+
+        bool collides(Actor a);
 };
 
 #endif
--- src/actor.cpp
+++ src/actor.cpp
@@ -1,31 +1,48 @@
 #include "actor.h"
 
 Actor::Actor(double x, double y, double width, double height) {
-	this->x = x;
-	this->y = y;
-	this->width = width;
-	this->height = height;
-	this->x_speed = 0;
-	this->y_speed = 0;
+    this->x = x;
+    this->y = y;
+    this->width = width;
+    this->height = height;
+    this->x_speed = 0;
+    this->y_speed = 0;
 }
 
 void Actor::run(void) {
-	x += x_speed;
-	y += y_speed;
+    x += x_speed;
+    y += y_speed;
 }
 
 double Actor::get_x(void) {
-	return x;
+    return x;
 }
 
 double Actor::get_y(void) {
-	return y;
+    return y;
+}
+
+double Actor::get_width(void) {
+    return width;
+}
+
+double Actor::get_height(void) {
+    return height;
 }
 
 double Actor::get_x_speed(void) {
-	return x_speed;
+    return x_speed;
 }
 
 double Actor::get_y_speed(void) {
-	return y_speed;
+    return y_speed;
+}
+
+bool Actor::collides(Actor a) {
+    return (
+        a.get_x() <= this->x + this->width &&
+        a.get_x() + a.get_width() >= this->x &&
+        a.get_y() <= this->y + this->height &&
+        a.get_y() + a.get_height() >= this->y
+    )
 }
