bbcbea029552f17f5438673ec015d88e54194063
ajmarra <ajmarra125@gmail.com>
2019-11-07 20:55:12 -0500

    trying to merge

--- include/actor.h
+++ include/actor.h
@@ -1,9 +1,6 @@
 #ifndef ACTOR_H
 #define ACTOR_H
 
-#include "math.h"
-#define PI 3.14159265
-
 enum class ActorType { ROOM, EXIT, FRED, ENEMY, HEALTH, WEAPON, BULLET, TRAP };
 
 class Actor {
@@ -11,13 +8,14 @@ class Actor {
         /**
          * x and y -- the coordinates of the top left pixel
          * width and height -- the number of pixels on the sides
+		 * speed -- magnitude of the speed of the actor
          */ 
         double x, y, xSpeed, ySpeed, width, height;
         int direction;
         ActorType type;
 
     public:
-		Actor() { };
+		Actor() { };// Do not use default constructor. Left here because Item class currently uses it.
         
         Actor(ActorType type, double x, double y, double width, double height);
         
@@ -25,62 +23,75 @@ class Actor {
 
         ActorType getType() { return this->type; };
         
-        double getX(void) { return x; };
+        double getX(void) { return this->x; };
         
-        double getY(void) { return y; };
+        double getY(void) { return this->y; };
 
 		void setXY(double x, double y) {
 			this->x = x;
 			this->y = y;
 		};
 
-        double getCenterX(void) { return (x + width / 2); };
+        double getCenterX(void) { return (this->x + this->width / 2); };
 
-        double getCenterY(void) { return (y + height / 2); };
+        double getCenterY(void) { return (this->y + this->height / 2); };
         
-        double getWidth(void) { return width; };
+        double getWidth(void) { return this->width; };
 
-        double getHeight(void) { return height; };
+        double getHeight(void) { return this->height; };
 
         double getXSpeed(void) { return xSpeed; };
 
-        void setXSpeed(int s) { this->xSpeed = s; };
-        
-        double getYSpeed(void) { return ySpeed; };
+		double getYSpeed(void) { return ySpeed; };
 
-        void setYSpeed(int s) { this->ySpeed = s; };
+		double getSpeed(void);
 
-        /**
-         * Sets the direction of movement.
-         * 
-         * d -- direction
-         */
-        void setDirection(int d) { direction = d; };
+		double getDirection(void);
 
-        int getDirection(void) { return direction; };
+		/**
+				 * Sets the orientation of the actor.
+				 *
+				 * d -- direction to face
+				 */
+		void setOrientation(int d) { this->orientation = d; };
 
-        /**
-         * Sets the orientation of the actor.
-         * 
-         * x -- x coordinate to face
-         * y -- y coordinate to face
-         */
-        void setFacing(int x, int y);
+		/**
+		 * Sets the orientation of the actor.
+		 *
+		 * x -- x coordinate to face
+		 * y -- y coordinate to face
+		 */
+		void setOrientation(int x, int y);
 
-        /**
-         * Sets the orientation of the actor.
-         * 
-         * a -- the actor to face towards
-         */
-        void setFacing(Actor a);
+		/**
+		 * Sets the orientation of the actor.
+		 *
+		 * a -- the actor to face towards
+		 */
+		void setOrientation(Actor a);
 
-        /**
-         * Calculates if Actor a is touching this actor.
-         * (the outer pixels are next to eachother, or the actors are overlapping).
-         * 
-         * a -- the actor for comparison
-         */
-        bool collides(Actor a);
+		/**
+		 * Returns the orientation of the actor.
+		 */
+		int getOrientation() { return this->orientation; };
+
+		/**
+		 * Calculates if square Actor a is touching this square actor.
+		 * (the outer pixels are next to eachother, or the actors are overlapping).
+		 *
+		 * a -- the actor for comparison
+		 */
+		bool collidesSquare(Actor a);
+
+		/**
+		 * Calculates if circular Actor a is touching this circular actor.
+		 * (the outer pixels are next to eachother, or the actors are overlapping).
+		 *
+		 * a -- the actor for comparison
+		 */
+		bool collidesCircle(Actor a);
+
+		bool liesInsideSquare(Actor a);
 };
 
 #endif
