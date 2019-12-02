d2bb489565a03ac5bea6db86330ef5cd5aa028e1
aaronamk <amklein@email.wm.edu>
2019-12-01 22:17:47 -0500

    Seg fault annoyance.

---
+++ src/range_enemy_view.cpp
@@ -0,0 +1,18 @@
+#include <list>
+#include <memory>
+#include <math.h>
+#include "view.h"
+#include "enemy_view.h"
+#include "range_enemy_view.h"
+#include "fred.h"
+#include "enemy.h"
+#include "room.h"
+#define PI 3.14159265
+
+RangeEnemyView::RangeEnemyView(std::shared_ptr<MasterLogic> logic, std::shared_ptr<Enemy> enemy)
+	: EnemyView(logic, enemy) { }
+
+void RangeEnemyView::update(float delta) {
+    if (this->enemy->collidesCircle(*this->logic->getCurrentRoom()->getFred(), 150));
+    else this->findFred();
+}
