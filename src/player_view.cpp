d88a41ac5951712002b864068ccc6261d84cff3d
ajmarra <ajmarra125@gmail.com>
2019-12-02 00:07:08 -0500

    Added speed boost sprite and made it dropable by enemies

--- src/player_view.cpp
+++ src/player_view.cpp
@@ -47,6 +47,8 @@ PlayerView::PlayerView(std::shared_ptr<MasterLogic> logic, std::shared_ptr<Fred>
 
     shield_image.spriteMap.loadFromFile("../resources/shield.png");
 
+    speed_image.spriteMap.loadFromFile("../resources/speed.png");
+
 
     EnemySprite1.spriteMap.loadFromFile("../resources/alienwalk.png");
     EnemySprite1.spriteFrame.top = 64;//x
@@ -223,7 +225,7 @@ void PlayerView::drawActor(Actor& a) {
     case ActorType::SPEED_BOOST:
     {
         sf::RectangleShape itemShape(sf::Vector2f(a.getWidth(), a.getHeight()));
-        //itemShape.setTexture(&shield_image.spriteMap);
+        itemShape.setTexture(&speed_image.spriteMap);
         itemShape.setPosition(a.getX(), a.getY());
         this->window->draw(itemShape);
     }
